4b4750f5871c394817380f03cbdd712421edc2b1
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-01-20 15:59:57 -0300

    fix: organization

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ const {middlewareGlobal } = require('./src/middlewares/middleware.js');
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, "public"))); 
-app.use(middlewareGlobal);
-app.set("view engine", "ejs");
+
 app.set("views", path.resolve(__dirname, "src","views"));
+app.set("view engine", "ejs");
 
 app.use(routes);
+app.use(middlewareGlobal);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
